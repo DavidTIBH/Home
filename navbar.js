3976e8efe5dd9d6c0ca1207f98845c8a6cc4876d
Mister Tech <157316303+DavidTIBH@users.noreply.github.com>
2024-05-25 18:55:17 -0300

    ARRUMEINAVBARACESSORESTRITO

--- navbar.js
+++ navbar.js
@@ -42,7 +42,7 @@ document.querySelectorAll('nav a').forEach(anchor => {
 
 
 // IDs de acesso permitidos
-var allowedIDs = ["ID1", "ID2", "ID3"]; // Adicione quantos IDs quiser aqui
+var allowedIDs = ["TESTE", "TESTE2", "TESTE3"]; // Adicione quantos IDs quiser aqui
 
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent default form submission
