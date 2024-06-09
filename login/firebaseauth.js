f2938ccce2f7c2a5e32cc5d7813a293199e3c5e8
Mister Tech <157316303+DavidTIBH@users.noreply.github.com>
2024-06-09 00:41:40 -0300

    testeteste

--- login/firebaseauth.js
+++ login/firebaseauth.js
@@ -89,17 +89,3 @@
         }
     })
  })
-
- // Redefinição de Senha
-const resetForm = document.getElementById('reset-form');
-resetForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('reset-email').value;
-  sendPasswordResetEmail(auth, email)
-    .then(() => {
-      console.log("E-mail de redefinição de senha enviado!");
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
-});
