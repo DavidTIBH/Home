822890fb08b2e01daa4bf58b3d581a7b8f9527ed
Mister Tech <157316303+DavidTIBH@users.noreply.github.com>
2024-06-09 00:02:35 -0300

    teste

--- login/firebaseauth.js
+++ login/firebaseauth.js
@@ -89,18 +89,3 @@
         }
     })
  })
-
-
-
-  const email = document.getElementById('email').value;
-  const auth = getAuth();
-  sendPasswordResetEmail(auth, email)
-    .then(() => {
-      // Password reset email sent!
-      // ..
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
