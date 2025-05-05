4ad3ae418950dca2299e507df1123a6e26d34b9b
Mister Tech <157316303+DavidTIBH@users.noreply.github.com>
2025-05-05 20:31:12 -0300

    teste2

--- navbar.js
+++ navbar.js
@@ -41,4 +41,39 @@ document.querySelectorAll('[href^="#"]').forEach(anchor => {
 });
 
 
- 
+
+
+//Efeito do SLIDE
+const slides = document.querySelector('.slides');
+const dots = document.querySelectorAll('.dot');
+let currentIndex = 0;
+const totalSlides = dots.length;
+
+function showSlide(index) {
+  if (index >= totalSlides) {
+    currentIndex = 0;
+  } else if (index < 0) {
+    currentIndex = totalSlides - 1;
+  } else {
+    currentIndex = index;
+  }
+
+  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+  dots.forEach(dot => dot.classList.remove('active'));
+  dots[currentIndex].classList.add('active');
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    showSlide(index);
+  });
+});
+
+showSlide(currentIndex);
+setInterval(nextSlide, 7000); // Troca a cada 6 segundos
+//acaba o efeito do slide
